7500aee9b534d25c3604764b49047e25822d3df8
Michał Piotrkowski <michal.piotrkowski@protonmail.com>
2025-06-01 01:53:08 +0200

    feat: add basic structure for coupon

---
+++ oddajze/oddajze.client/src/app/features/coupon/pages/coupon.route.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { CouponComponent } from './coupon.component';
+
+export const COUPON_ROUTES: Route[] = [
+  { path: '', component: CouponComponent, title: 'Coupon' },
+];
