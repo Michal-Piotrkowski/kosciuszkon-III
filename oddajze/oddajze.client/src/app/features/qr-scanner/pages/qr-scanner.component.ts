abdc9fa270fbde5e1041a1d6bce8f27398be7c84
Michał Piotrkowski <michal.piotrkowski@protonmail.com>
2025-06-01 00:13:41 +0200

    feat: add method to receive data from API

--- oddajze/oddajze.client/src/app/features/qr-scanner/pages/qr-scanner.component.ts
+++ oddajze/oddajze.client/src/app/features/qr-scanner/pages/qr-scanner.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { BarcodeFormat } from '@zxing/library';
 import { CommonModule } from '@angular/common';
+import { QrApiService } from '../../../shared/services/qr-api.service';
 
 @Component({
   selector: 'app-qr-scanner',
@@ -12,12 +13,17 @@ import { CommonModule } from '@angular/common';
 })
 
 export class QrScannerComponent {
+  private qrApi: QrApiService = inject(QrApiService);
   qrResult: string | null = null;
   scannerEnabled = false;
   allowedFormats = [ BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX /*, ...*/ ];
   scannerKey = 0;
+  
   onScanSuccess($event: string) {
-    this.qrResult = $event;
+    this.qrApi.postQrCode($event).subscribe((response) => {
+      this.qrResult = response.data;
+      console.log('QR Code scanned successfully:', this.qrResult);
+    });
   }
 
   toggleScanner() {
