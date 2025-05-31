746d9042135091ae572e772bc5060ab56e08bb9b
Michał Piotrkowski <michal.piotrkowski@protonmail.com>
2025-05-31 20:27:49 +0200

    feat: add header, burger menu

--- oddajze/oddajze.client/src/app/app.component.ts
+++ oddajze/oddajze.client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
