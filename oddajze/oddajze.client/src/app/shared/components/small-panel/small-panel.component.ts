a1450df1392ea2438a7e9efad37e7fe484856493
jkbPolec <jkb.polec@gmail.com>
2025-05-31 22:02:27 +0200

    Added modular small panels to display images, banners

---
+++ oddajze/oddajze.client/src/app/shared/components/small-panel/small-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-panel',
+  imports: [],
+  templateUrl: './small-panel.component.html',
+  styleUrl: './small-panel.component.scss'
+})
+export class SmallPanelComponent {
+
+}
