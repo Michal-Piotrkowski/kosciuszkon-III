41355986768ff0a92f9f470d3cf6dc60b9a76eb0
Michał Piotrkowski <michal.piotrkowski@protonmail.com>
2025-05-31 21:13:28 +0200

    feat: add footer structure and styles

--- oddajze/oddajze.client/src/app/app.component.ts
+++ oddajze/oddajze.client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet, HeaderComponent],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
