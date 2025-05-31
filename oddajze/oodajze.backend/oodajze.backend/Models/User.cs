751adc11324499fc77fd73bc56c44d40a6d52d9a
ZuzannaZawartka <raf.zuzia7@gmail.com>
2025-05-31 18:22:02 +0200

    feat(seeder): add data seeder and password to user

--- oddajze/oodajze.backend/oodajze.backend/Models/User.cs
+++ oddajze/oodajze.backend/oodajze.backend/Models/User.cs
@@ -5,10 +5,11 @@ public class User
     public string Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string Email { get; set; }             
+    public string PasswordHash { get; set; } 
     public string Phone { get; set; }
-    public string Email { get; set; }
     public int TotalPoints { get; set; }
     public List<UserCoupon> RedeemedCoupons { get; set; } = new List<UserCoupon>(); 
     public DateTime JoinDate { get; set; }
-    public List<CollectionVisitQrData> CollectionHistory { get; set; } = new List<CollectionVisitQrData>();
+    public List<CollectionVisitQrData> CollectionVisitQrDataHistory { get; set; } = new List<CollectionVisitQrData>();
 }
