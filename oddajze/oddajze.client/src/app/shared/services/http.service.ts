504bdb3d60d3ddf5fff0965b7fc250147bbc3f05
Michał Piotrkowski <michal.piotrkowski@protonmail.com>
2025-05-31 23:47:20 +0200

    feat(api): add reusable HttpService with environment-based base URL

---
+++ oddajze/oddajze.client/src/app/shared/services/http.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from '../../../environment/environment';
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class HttpService {
+  get apiUrl(): string | undefined {
+    return  environment.apiUrl;
+  }
+
+  constructor(private http: HttpClient) {}
+
+  get(endpoint: string, options?: any): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${endpoint}`, options);
+  }
+
+  post(endpoint: string, data: any, options?: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/${endpoint}`, data, options).pipe(
+      catchError(error => {
+        console.error('Error occurred:', error);
+        return throwError('Something went wrong!');
+      })
+    );
+  }
+
+  put(endpoint: string, data: any, options?: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${endpoint}`, data, options);
+  }
+
+  delete(endpoint: string, options?: any): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${endpoint}`, options);
+  }
+}
