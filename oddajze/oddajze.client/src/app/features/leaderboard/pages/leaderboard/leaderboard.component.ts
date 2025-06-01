94cb09e43f88802942dca51ffaf8f84737cdc27e
jkbPolec <jkb.polec@gmail.com>
2025-06-01 03:35:32 +0200

    Finished styling leaderboard

--- oddajze/oddajze.client/src/app/features/leaderboard/pages/leaderboard/leaderboard.component.ts
+++ oddajze/oddajze.client/src/app/features/leaderboard/pages/leaderboard/leaderboard.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { SmallPanelComponent} from '../../../../shared/components/small-panel/small-panel.component';
+import { Component, OnInit } from '@angular/core';
+import { SmallPanelComponent } from '../../../../shared/components/small-panel/small-panel.component';
+import { HttpService } from '../../../../shared/services/http.service';
+import { RankingUser } from '../../../../shared/models/ranking-user.model';
+import {Observable} from 'rxjs';
+import {NgClass, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-leaderboard',
   imports: [
     SmallPanelComponent,
+    NgForOf,
+    NgClass,
   ],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.scss'
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
+  constructor(private http: HttpService) {}
+
+  ranking: RankingUser[] = [];
+
+  ngOnInit(): void {
+    this.getAllRankings().subscribe({
+      next: (data: RankingUser[]) => {
+        this.ranking = data;
+        this.ranking.forEach(user => {
+          console.log(`${user.id} Użytkownik: ${user.username}, Punkty: ${user.totalPoints}`);
+        });
+      },
+      error: (error) => console.error('Błąd podczas pobierania rankingów:', error)
+    })
+  }
+
+  getAllRankings(): Observable<RankingUser[]> {
+    return this.http.get('Users/ranking');
+  }
+
 
 }
---
+++ oddajze/oddajze.client/src/app/shared/models/ranking-user.model.ts
@@ -0,0 +1,6 @@
+export interface RankingUser {
+
+  id: number;
+  username: string;
+  totalPoints: number;
+}
