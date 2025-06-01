8c6f8a70a64ecffcaba45bae5a5f7c58742ab983
Michał Piotrkowski <michal.piotrkowski@protonmail.com>
2025-06-01 07:07:34 +0200

    fix: display of coupons and added logos urls

--- oddajze/oddajze.client/src/environment/environment.ts
+++ oddajze/oddajze.client/src/environment/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:5041/api',
     apiUrlSpecial: 'http://localhost:5041/',
+    apiUploads: 'http://localhost:5041/uploads/',
   };
   
